d731951927380184809e5f0816914f5bc02c97ea
Md. Abbas Ali <alabbashossain@gmail.com>
2021-02-10 23:52:53 +0600

    complate switch-bank

---
+++ js/index.js
@@ -0,0 +1,54 @@
+//Login click handler
+function forLoginButton() {
+    const loginArea = document.getElementById('login-area');
+    loginArea.style.display = 'none';
+    const transactionArea = document.getElementById('transaction-area');
+    transactionArea.style.display = 'block';
+}
+const loginButton = document.getElementById('login');
+loginButton.addEventListener('click', forLoginButton);
+
+//Deposit click handler
+function addDepositAmount() {
+    const depositAmount = document.getElementById('depositAmount').value;
+    const depositNumber = parseFloat(depositAmount);
+
+    if(depositNumber < 0){
+        alert("deposit number can not be negative.");
+    }
+    else{
+        updateSpanText("currentDeposit", depositNumber);
+        updateSpanText("currentBalance", depositNumber);
+    
+        document.getElementById('depositAmount').value = "";
+    }
+
+   
+}
+function updateSpanText(id, addedNumber) {
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const totalAmount = addedNumber + currentNumber;
+    document.getElementById(id).innerText = totalAmount;
+}
+
+const addDeposit = document.getElementById('addDeposit');
+addDeposit.addEventListener('click', addDepositAmount);
+
+
+//Withdraw click handler
+const addWithdrawButton = document.getElementById('addWithdraw');
+addWithdrawButton.addEventListener('click', withdrawHandler);
+
+function withdrawHandler() {
+    const withdrawAmount = getInputNumber('withdrawAmount');
+    updateSpanText('currentWithdraw', withdrawAmount);
+    updateSpanText('currentBalance', -1 * withdrawAmount)
+    document.getElementById('withdrawAmount').value = "";
+
+}
+function getInputNumber(id) {
+    const withdrawAmount = document.getElementById(id).value;
+    const amountNumber = parseFloat(withdrawAmount);
+    return amountNumber;
+}
